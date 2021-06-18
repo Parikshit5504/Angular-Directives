c81b52a6ab678025befea825aa84f59be18dbefd
Parikshit5504 <desaip932@gmail.com>
2021-06-18 06:59:30 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo8';
+
+  constructor(){ }
+
+// show = true;
+// hide = false;
+// name = 'PARIKSHIT';
+// course = 'html';
+// lang = ['html', 'php', 'java', 'python', 'kotlin']
+user = [
+  {name:'Parikshit', email:'parikshit@gmail.com'},
+  {name:'revati', email:'revati@gmail.com'},
+  {name:'pawan', email:'pawan@gmail.com'},
+  {name:'ram', email:'ram@gmail.com'}
+  ]
 }
+
